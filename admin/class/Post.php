c53e5e0418f3ff1c41bc2627d1c938dabd6f56e0
najiya muhammed mansoor <68012131+NajiyaHannath@users.noreply.github.com>
2020-09-17 15:22:58 +0530

    Update Post.php

---
+++ admin/class/Post.php
@@ -0,0 +1,41 @@
+<section id="main">
+<div class="container">
+<div class="row">	
+<?php include "left_menus.php"; ?>
+<div class="col-md-9">
+<div class="panel panel-default">
+<div class="panel-heading">
+<h3 class="panel-title">Post Listing</h3>
+</div>
+<div class="panel-body">
+<div class="panel-heading">
+<div class="row">
+<div class="col-md-10">
+<h3 class="panel-title"></h3>
+</div>
+<div class="col-md-2" align="right">
+<a href="compose_post.php" 
+class="btn btn-default btn-xs">Add New</a>				
+</div>
+</div>
+</div>
+<table id="postsList" class="table table-bordered table-striped">
+<thead>
+<tr>
+<th>Title</th>
+<th>Category</th>
+<th>User</th>
+<th>Status</th>	
+<th>Created</th>
+<th>Updated</th>															
+<th></th>
+<th></th>	
+</tr>
+</thead>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
