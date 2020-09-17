2bc5653c948fa0457db8d6e74d440a25e0c79184
najiya muhammed mansoor <68012131+NajiyaHannath@users.noreply.github.com>
2020-09-17 11:30:44 +0530

    Update category.php

---
+++ admin/category.php
@@ -0,0 +1,12 @@
+<div class="panel-body">
+	<div class="panel-heading">
+		<div class="row">
+			<div class="col-md-10">
+				<h3 class="panel-title"></h3>
+			</div>
+			<div class="col-md-2" align="right">
+				<a href="add_categories.php" 
+class="btn btn-default btn-xs">Add New</a>				
+			</div>
+		</div>
+	</div>
